6a635ba1f92d9c3f12767af34f2d848b66101577
andrejbsvc <andrej.barosevcic@gmail.com>
2025-10-03 10:16:38 +0200

    Übung2 Datentypen DONE

---
+++ Typescript/Übung2/Übung2.ts
@@ -0,0 +1,11 @@
+let a: number = 4;
+let title: string = "hansi";
+let isMale: boolean = true;
+
+function sayHello(title:string):void {
+    console.log(`My name is ${title} `);
+
+}
+
+sayHello("Andrej");
+

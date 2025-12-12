fa7280c331cad40e0fb03cd21fa20d8b95bd7c7c
andrejbsvc <andrej.barosevcic@gmail.com>
2025-12-12 21:57:36 +0100

    Aufgabe 3 DONE

--- React/vite-project/src/App.tsx
+++ React/vite-project/src/App.tsx
@@ -2,18 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-import "./App.css";
+
 import Card from "./components/Card";
 
 export default function App() {
   return (
     <div>
       <h1>HELLO</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    
+      <div className ="grid grid-cols-2 w-80 gap-4">
+        
+      <Card title="Susi" description="Super Lehrerin"/>
+      <Card title="Hans" description="Super Typ"/>
+      <Card title="Sepp" description="Echt gut"/>
+      </div>
+
     </div>
+    
   );
 }
--- React/vite-project/src/components/Card.tsx
+++ React/vite-project/src/components/Card.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {
+  title: string;
+  description: string;
+};
 
-export default function Card({}: Props) {
+export default function Card({ title, description }: Props) {
   return (
-    <div>Card</div>
+    <div className="border bg-red-200 p-4 items-center">
+    <h2 className="font-bold">{title}</h2>
+    <p>{description}</p>
+  </div>
   )
+
+
 }

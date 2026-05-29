bd72604cdc49495c01103b85fa86b428159d260f
andrejbsvc <andrej.barosevcic@gmail.com>
2026-05-29 10:38:42 +0200

    Deleted Compiled Files and added a .gitignore

--- Typescript/04_Klasse/01_4aWI_Patterns/MyGame.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=MyGame.d.ts.map
--- Typescript/04_Klasse/01_4aWI_Patterns/actors/SuperCircle.d.ts
+++
@@ -1,8 +0,0 @@
-import { Circle } from "./Circle.js";
-export declare class SuperCircle extends Circle {
-    speed: number;
-    constructor(x: number, y: number, radius: number, speed?: number);
-    sayHello(): void;
-    move(delta: number): void;
-}
-//# sourceMappingURL=SuperCircle.d.ts.map
